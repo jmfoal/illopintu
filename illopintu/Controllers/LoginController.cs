e61e4adea055a9c17025bd24bfecb3d1ba33d663
jmfoal@gmail.com <jmfoal@gmail.com>
2017-01-27 14:03:58 +0100

    login

--- illopintu/Controllers/LoginController.cs
+++ illopintu/Controllers/LoginController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using illopintu.Models;
 
 namespace illopintu.Controllers
 {
     public class LoginController : Controller
     {
+        Login modelo = new Login();
         [HttpGet]
         // GET: Login
         public ActionResult Index()
@@ -15,11 +17,38 @@ namespace illopintu.Controllers
             return View();
         }
 
+        public ActionResult errorLogin()
+        {
+            return View();
+        }
+
         [HttpPost]
         public ActionResult login(String user,String password)
         {
-            
+            ActionResult redireccion;
+            if (modelo.comprobarUser(user, password))
+            {
+                Session["username"] = user;
+                redireccion = RedirectToAction("index","game");
+            } else
+            {
+                redireccion = RedirectToAction("errorLogin");
+            }
+            return redireccion;
         }
         
+        public ActionResult register(String user, String password)
+        {
+            ActionResult redireccion;
+            if (modelo.registrarUser(user,password))
+            {
+                 redireccion =  this.login(user, password);
+            }
+            else
+            {
+                redireccion = this.errorLogin();
+            }
+            return redireccion;
+        }        
     }
 }
--- illopintu/Models/Login.cs
+++ illopintu/Models/Login.cs
@@ -3,16 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.IO;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace illopintu.Models
 {
     public class Login
     {
+        MD5CryptoServiceProvider cifrador = new MD5CryptoServiceProvider();
+        String path = HttpContext.Current.Server.MapPath("~/textos/users.txt");
         public Boolean comprobarUser(String username, String password)
         {
+            byte[] cadena = UTF8Encoding.UTF8.GetBytes(password);
+            cadena = cifrador.ComputeHash(cadena);
+            password = Convert.ToBase64String(cadena);
+
             Boolean correcto = false;
 
-            using (StreamReader sr = new StreamReader("../textos/users.txt"))
+            using (StreamReader sr = new StreamReader(path))
             {
                 while (!sr.EndOfStream)
                 {
@@ -25,6 +33,40 @@ namespace illopintu.Models
                     }
                 }
                 sr.Close();
+            }
+            return correcto;
+        }
+
+        public Boolean registrarUser(String username,String password)
+        {
+            byte[] cadena = UTF8Encoding.UTF8.GetBytes(password);
+            cadena = cifrador.ComputeHash(cadena);
+            password = Convert.ToBase64String(cadena);
+
+            Boolean existe = false;
+            Boolean correcto = false;
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    String linea = sr.ReadLine();
+                    String[] usuario = linea.Split(':');
+
+                    if (usuario[0] == username)
+                    {
+                        existe = true;
+                    }
+                }
+                sr.Close();
+            }
+            if (!existe)
+            {
+                using (StreamWriter sw = new StreamWriter(path, true))
+                {
+                    sw.WriteLine(username + ":" + password);
+                }
+                correcto = true;
             }
             return correcto;
         }
