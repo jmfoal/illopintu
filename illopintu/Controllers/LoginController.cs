e8bc0810027c89f8a49c060bd8c5e423155e6ad6
jmfoal@gmail.com <jmfoal@gmail.com>
2017-01-27 12:31:23 +0100

    a

---
+++ illopintu/Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace illopintu.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult login(String user,String password)
+        {
+            
+        }
+        
+    }
+}
---
+++ illopintu/Models/Login.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+
+namespace illopintu.Models
+{
+    public class Login
+    {
+        public Boolean comprobarUser(String username, String password)
+        {
+            Boolean correcto = false;
+
+            using (StreamReader sr = new StreamReader("../textos/users.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    String linea = sr.ReadLine();
+                    String[] usuario = linea.Split(':');
+
+                    if(usuario[0] == username && usuario[1] == password)
+                    {
+                        correcto = true;
+                    }
+                }
+                sr.Close();
+            }
+            return correcto;
+        }
+    }
+}
