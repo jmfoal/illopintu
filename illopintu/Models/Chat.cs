918c36db66bfb7bbce0757593761b51894641ac9
jmfoal@gmail.com <jmfoal@gmail.com>
2017-01-31 10:03:32 +0100

    chat

--- illopintu/Models/Chat.cs
+++ illopintu/Models/Chat.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 
@@ -9,6 +10,34 @@ namespace illopintu.Models
     {
         String path = HttpContext.Current.Server.MapPath("~/textos/chat.txt");
 
+        public void setChat(string linea)
+        {            
+            using(StreamWriter sw = new StreamWriter(path, true))
+            {
+                sw.WriteLine(linea);
+            }
+        }
+
+        public List<string> getChat()
+        {
+            List < string > chat = new List<string>();
+
+            using(StreamReader sr = new StreamReader(path))
+            {
+                while (!sr.EndOfStream) {
+                    if (chat.Count < 20)
+                    {
+                        chat.Add(sr.ReadLine());
+                    }
+                    else
+                    {
+                        chat.RemoveAt(0);
+                        chat.Add(sr.ReadLine());
+                    }
+                }
+            }
+            return chat;
+        }
         
     }
 }
